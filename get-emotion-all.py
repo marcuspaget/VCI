e7973a7265f48ed1d259210eb86598f4351bb813
Mark Paget <marcuspaget@gmail.com>
2021-06-20 19:40:20 +0800

    Add files via upload

---
+++ get-emotion-all.py
@@ -0,0 +1,16 @@
+import os
+import sys
+import pathlib
+from fer import FER
+import matplotlib.pyplot as plt 
+
+#path='/Users/mark/Coding/ZoomAttentionRating/2/FaceDetect/images'
+path=sys.argv[1]
+
+img_files = [f for f in os.listdir(path) if f.endswith('.jpg')]
+
+for imgfile in img_files:
+	img = plt.imread(imgfile)
+	detector = FER(mtcnn=True)
+	emotion, score = detector.top_emotion(img)
+	print(imgfile,emotion,score)
